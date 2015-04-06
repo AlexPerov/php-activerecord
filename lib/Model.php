efc26b7bf2b6c962db3c81d6436261e24bf67b82
Max Loeb <shmax@shmax.com>
2015-04-05 20:21:48 -0700

    fix eager loading caching

--- lib/Model.php
+++ lib/Model.php
@@ -1609,7 +1609,7 @@ class Model
 	 * @param mixed $pks primary keys
 	 * @return array
 	 */
-	protected static function get_models_from_cache($pks)
+	protected static function get_models_from_cache($pks, $options)
 	{
 		$models = array();
 		$table = static::table();
@@ -1621,7 +1621,7 @@ class Model
 
 		foreach($pks as $pk)
 		{
-			$options = array('conditions' => static::pk_conditions($pk));
+			$options['conditions'] = static::pk_conditions($pk);
 			$models[] = Cache::get($table->cache_key_for_model($pk), function() use ($table, $options)
 			{
 				$res = $table->find($options);
@@ -1651,7 +1651,7 @@ class Model
 
 		if($table->cache_individual_model)
 		{
-			$list = static::get_models_from_cache($values);
+			$list = static::get_models_from_cache($values, $options);
 		}
 		else
 		{
